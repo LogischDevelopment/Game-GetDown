2fb7bb1754332ca2c65752a7271d7106932efe42
Timo <timokeutgen@gmx.de>
2025-07-20 22:32:18 +0200

    Prevent block breaking for non-creative players in `BlockBreakListener`.

--- src/main/java/tv/logisch/game/listener/BlockBreakListener.java
+++ src/main/java/tv/logisch/game/listener/BlockBreakListener.java
@@ -1,5 +1,6 @@
 package tv.logisch.game.listener;
 
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -8,6 +9,7 @@ public class BlockBreakListener implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
+        if(e.getPlayer().getGameMode().equals(GameMode.CREATIVE)) return;
         e.setCancelled(true);
     }
 
