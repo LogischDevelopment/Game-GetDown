43b68bf07f100b75626aa3b8e40a4e0eee1e5db3
Timo <timokeutgen@gmx.de>
2025-07-20 22:33:24 +0200

    Add `PlayerInteractListener` to handle right-click interactions for opening shop GUI or anvil inventory

---
+++ src/main/java/tv/logisch/game/listener/PlayerInteractListener.java
@@ -0,0 +1,39 @@
+package tv.logisch.game.listener;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.MenuType;
+import org.bukkit.persistence.PersistentDataType;
+import tv.logisch.game.GetDown;
+import tv.logisch.game.gui.shop.WeaponGUI;
+import tv.logisch.game.manager.GameManager;
+
+public class PlayerInteractListener implements Listener {
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        if(!e.getAction().isRightClick()) return;
+        if(e.getItem() == null) return;
+        if(!e.getItem().getPersistentDataContainer().has(GameManager.get().shopKey())) return;
+
+        String s = e.getItem().getPersistentDataContainer().get(GameManager.get().shopKey(), PersistentDataType.STRING);
+        if(s == null || s.isEmpty()) return;
+
+        if(s.equalsIgnoreCase("open")) {
+            WeaponGUI.get(e.getPlayer()).open();
+            return;
+        }
+        if(s.equalsIgnoreCase("anvil")) {
+            Inventory inv = Bukkit.createInventory(e.getPlayer(), InventoryType.ANVIL, Component.text(GetDown.instance().prefix() + "Anvil"));
+            e.getPlayer().openInventory(inv);
+            return;
+        }
+
+    }
+
+}
