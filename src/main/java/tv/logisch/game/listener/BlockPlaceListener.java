35608f8abcc82e58c37bb311b37a164b472f7800
Timo <timokeutgen@gmx.de>
2025-07-20 22:32:22 +0200

    Add TNT placement mechanic and restrict block usage for non-creative players

--- src/main/java/tv/logisch/game/listener/BlockPlaceListener.java
+++ src/main/java/tv/logisch/game/listener/BlockPlaceListener.java
@@ -1,7 +1,10 @@
 package tv.logisch.game.listener;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -11,10 +14,20 @@ public class BlockPlaceListener implements Listener {
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
-        if (e.getBlockPlaced().getType() == Material.BRICKS) {
+        if(e.getPlayer().getGameMode().equals(GameMode.CREATIVE)) return;
+        if (e.getBlockPlaced().getType().equals(Material.BRICKS)) {
             Bukkit.getScheduler().runTaskLater(GetDown.instance(), () -> {
                 e.getBlockPlaced().setType(Material.AIR);
             }, 7*20L);
+            return;
+        }
+        if(e.getBlockPlaced().getType().equals(Material.TNT)) {
+            e.getBlockPlaced().setType(Material.AIR);
+
+            Location loc = e.getBlockPlaced().getLocation().add(0.5, 0, 0.5);
+            TNTPrimed tnt = (TNTPrimed) loc.getWorld().spawn(loc, TNTPrimed.class);
+            tnt.setSource(e.getPlayer());
+            tnt.setFuseTicks(80); // 4 seconds
             return;
         }
         e.setCancelled(true);
