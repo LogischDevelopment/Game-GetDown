de009a0473bbd679433082ccf4827723ea05cf20
Timo <timokeutgen@gmx.de>
2025-07-20 22:33:34 +0200

    Enhance `PlayerMoveListener` to handle player arrival logic, reward distribution, game phase transitions, and block interaction effects.

--- src/main/java/tv/logisch/game/listener/PlayerMoveListener.java
+++ src/main/java/tv/logisch/game/listener/PlayerMoveListener.java
@@ -1,13 +1,16 @@
 package tv.logisch.game.listener;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.scheduler.BukkitTask;
 import tv.logisch.game.GetDown;
 import tv.logisch.game.enums.GameState;
 import tv.logisch.game.manager.GameManager;
@@ -15,11 +18,13 @@ import tv.logisch.game.objects.EffectSetting;
 import tv.logisch.game.objects.GameEffect;
 import tv.logisch.game.objects.ItemSetting;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class PlayerMoveListener implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
-        if(e.getFrom().getBlockX() == e.getTo().getBlockX() && e.getFrom().getBlockZ() == e.getTo().getBlockZ()) {
+        if(e.getFrom().getBlockX() == e.getTo().getBlockX() && e.getFrom().getBlockY() == e.getTo().getBlockY() && e.getFrom().getBlockZ() == e.getTo().getBlockZ()) {
             return;
         }
 
@@ -29,8 +34,7 @@ public class PlayerMoveListener implements Listener {
         }
 
         if(GameManager.get().state().equals(GameState.WAITING) || GameManager.get().state().equals(GameState.SHOPPING)) {
-            int y = e.getTo().getBlockY();
-            if(y-10 < e.getPlayer().getWorld().getSpawnLocation().getBlockY()) {
+            if(e.getTo().getBlockY() < 50) {
                 e.setTo(e.getPlayer().getWorld().getSpawnLocation());
             }
             return;
@@ -41,12 +45,42 @@ public class PlayerMoveListener implements Listener {
             if(e.getPlayer().getLocation().getBlockY() <= 0) {
                 Bukkit.getScheduler().runTaskLater(GetDown.instance(), () -> {
                     if(GameManager.get().state().equals(GameState.RUNNING)) {
-                        if(e.getPlayer().getLocation().getBlockY() <= 0) {
+                        if(e.getPlayer().getLocation().getBlockY() <= 1) {
+                            if(GameManager.get().playersFinished.contains(e.getPlayer())) return;
                             GameManager.get().playersFinished.add(e.getPlayer());
-                            // TODO: implement player message and global message
+                            int finishedCount = GameManager.get().playersFinished.size();
+                            if(finishedCount == 1) {
+                                GameManager.get().playerCoinManager().addCoins(e.getPlayer(), 300);
+                            } else if(finishedCount == 2) {
+                                GameManager.get().playerCoinManager().addCoins(e.getPlayer(), 200);
+                            } else if(finishedCount == 3) {
+                                GameManager.get().playerCoinManager().addCoins(e.getPlayer(), 100);
+                            }
 
-                            if(GameManager.get().playersFinished.size() >= 3) {
-                                GameManager.get().startShopping();
+                            Bukkit.getOnlinePlayers().forEach(p -> {
+                                p.sendMessage(Component.text(GetDown.instance().prefix() + "§f"+e.getPlayer().getName()+" §7ist unten angekommen! ("+finishedCount+"/3)"));
+                                p.playSound(p, Sound.ENTITY_ENDER_DRAGON_AMBIENT, 1.0f, 1.0f);
+                            });
+
+                            if(finishedCount >= 3 || finishedCount == Bukkit.getOnlinePlayers().size()) {
+                                GameManager.get().state(GameState.FINISHED);
+
+                                AtomicInteger seconds = new AtomicInteger(10);
+                                AtomicInteger taskId = new AtomicInteger(0);
+                                taskId.set(Bukkit.getScheduler().runTaskTimer(GetDown.instance(), () -> {
+                                    if(seconds.get() <= 0) {
+                                        GameManager.get().startShopping();
+                                        Bukkit.getScheduler().cancelTask(taskId.get());
+                                    } else {
+                                        if(seconds.get() == 10 || seconds.get() <= 5) {
+                                            Bukkit.getOnlinePlayers().forEach(p -> {
+                                                p.sendMessage(Component.text(GetDown.instance().prefix() + "Die Shopping Phase beginnt in §f" + seconds.get() + " §7Sekunden!"));
+                                                p.playSound(p, Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 1.0f);
+                                            });
+                                        }
+                                    }
+                                    seconds.getAndDecrement();
+                                }, 0L, 20L).getTaskId());
                             }
                         }
                     }
@@ -61,6 +95,8 @@ public class PlayerMoveListener implements Listener {
                 int max = 55;
                 int coins = (int) (Math.random() * (max - min + 1)) + min;
                 GameManager.get().playerCoinManager().addCoins(e.getPlayer(), coins);
+                e.getPlayer().sendMessage(Component.text(GetDown.instance().prefix() + "§aDu hast §f" + coins + " §aCoins bekommen!"));
+                block.setType(Material.YELLOW_CONCRETE);
             } else if(block.getType().equals(Material.DIAMOND_BLOCK)) {
                 int min = 75;
                 int max = 115;
@@ -70,16 +106,24 @@ public class PlayerMoveListener implements Listener {
                 ItemStack randomItem = ItemSetting.getRandomItem();
                 if(randomItem != null) {
                     e.getPlayer().getInventory().addItem(randomItem);
+                    e.getPlayer().sendMessage(Component.text(GetDown.instance().prefix() + "§aDu hast ein Item erhalten: §f" + randomItem.getType().name()));
                 }
+                block.setType(Material.GRAY_CONCRETE);
             } else if(block.getType().equals(Material.OBSIDIAN)) {
                 if(Math.random() * 100 < GameManager.get().percentage()) {
                     block.setType(Material.SLIME_BLOCK);
+                    e.getPlayer().sendMessage(Component.text(GetDown.instance().prefix() + "§aDu bist auf einen Slime Block gefallen!"));
+                } else {
+                    block.setType(Material.COBBLESTONE);
+                    e.getPlayer().sendMessage(Component.text(GetDown.instance().prefix() + "§cDu bist auf einen normalen Block gefallen!"));
                 }
-            } else if(block.getType().equals(Material.REDSTONE)) {
+            } else if(block.getType().equals(Material.LAPIS_BLOCK)) {
                 GameEffect effect = EffectSetting.getRandomEffect();
                 if(effect != null) {
                     e.getPlayer().addPotionEffect(new PotionEffect(effect.type(), effect.duration() * 20, effect.amplifier()));
+                    e.getPlayer().sendMessage(Component.text(GetDown.instance().prefix() + "§aDu hast einen Effekt erhalten: §f" + effect.type().translationKey() + " §7(" + effect.duration() + " Sekunden, Stufe " + (effect.amplifier() + 1) + ")"));
                 }
+                block.setType(Material.CYAN_CONCRETE);
             }
 
         }
