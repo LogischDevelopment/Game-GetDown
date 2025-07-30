5b2a45d88e6e857218ca67778c7f25051231b101
Timo <timokeutgen@gmx.de>
2025-07-30 19:14:11 +0200

    #6 Simplify dropping phase cooldown logic with `stopDroppingPhaseCooldown` integration.

--- src/main/java/tv/logisch/game/listener/PlayerMoveListener.java
+++ src/main/java/tv/logisch/game/listener/PlayerMoveListener.java
@@ -55,6 +55,7 @@ public class PlayerMoveListener implements Listener {
                             if(finishedCount == 1) {
                                 GameManager.get().playerCoinManager().addCoins(e.getPlayer(), 300);
                                 e.getPlayer().sendTitlePart(TitlePart.TITLE, Component.text("§6+300"));
+                                GameManager.get().startDroppingPhaseCooldown();
                             } else if(finishedCount == 2) {
                                 GameManager.get().playerCoinManager().addCoins(e.getPlayer(), 200);
                                 e.getPlayer().sendTitlePart(TitlePart.TITLE, Component.text("§6+200"));
@@ -69,24 +70,7 @@ public class PlayerMoveListener implements Listener {
                             });
 
                             if(finishedCount >= 3 || finishedCount == Bukkit.getOnlinePlayers().size()) {
-                                GameManager.get().state(GameState.FINISHED);
-
-                                AtomicInteger seconds = new AtomicInteger(15);
-                                AtomicInteger taskId = new AtomicInteger(0);
-                                taskId.set(Bukkit.getScheduler().runTaskTimer(GetDown.instance(), () -> {
-                                    if(seconds.get() <= 0) {
-                                        GameManager.get().startShopping();
-                                        Bukkit.getScheduler().cancelTask(taskId.get());
-                                    } else {
-                                        if(seconds.get() == 15 || seconds.get() == 10 || seconds.get() <= 5) {
-                                            Bukkit.getOnlinePlayers().forEach(p -> {
-                                                p.sendMessage(Component.text(GetDown.instance().prefix() + "Die Shopping Phase beginnt in §f" + seconds.get() + " §7Sekunden!"));
-                                                p.playSound(p, Sound.BLOCK_NOTE_BLOCK_PLING, 1.0f, 1.0f);
-                                            });
-                                        }
-                                    }
-                                    seconds.getAndDecrement();
-                                }, 0L, 20L).getTaskId());
+                                GameManager.get().stopDroppingPhaseCooldown();
                             }
                         }
                     }
