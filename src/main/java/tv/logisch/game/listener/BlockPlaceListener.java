d64c5b9d88d95f0ce0fcad311d431a40b3ce4179
Fb1an <raffeinerfabian0908@gmail.com>
2025-08-07 22:48:12 +0200

    Added Particle and Sound on Brick Break

--- src/main/java/tv/logisch/game/listener/BlockPlaceListener.java
+++ src/main/java/tv/logisch/game/listener/BlockPlaceListener.java
@@ -1,9 +1,7 @@
 package tv.logisch.game.listener;
 
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,9 +14,21 @@ public class BlockPlaceListener implements Listener {
     public void onBlockPlace(BlockPlaceEvent e) {
         if(e.getPlayer().getGameMode().equals(GameMode.CREATIVE)) return;
         if (e.getBlockPlaced().getType().equals(Material.BRICKS)) {
+            // Bukkit.getScheduler().runTaskLater(GetDown.instance(), () -> {
+            //     e.getBlockPlaced().setType(Material.AIR);
+            // }, 7*20L);
+
+            Block block = e.getBlock();
+            Location location = block.getLocation();
+
             Bukkit.getScheduler().runTaskLater(GetDown.instance(), () -> {
-                e.getBlockPlaced().setType(Material.AIR);
-            }, 7*20L);
+                location.getWorld().spawnParticle(Particle.BLOCK_CRUMBLE, location.add(0.5, 0.5, 0.5), 20, block.getBlockData());
+
+                location.getWorld().playSound(location, Sound.BLOCK_STONE_BREAK, 1.0f, 1.0f);
+
+                block.setType(Material.AIR);
+            }, 7 * 20);
+
             return;
         }
         if(e.getBlockPlaced().getType().equals(Material.TNT)) {
