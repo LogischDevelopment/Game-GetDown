81ebd718f1f501dd5db15b07a22a80bb1f938a60
Fb1an <raffeinerfabian0908@gmail.com>
2025-08-08 10:54:39 +0200

    Fixed start/stopDroppingPhaseCooldown for only 1 Player

--- src/main/java/tv/logisch/game/listener/PlayerMoveListener.java
+++ src/main/java/tv/logisch/game/listener/PlayerMoveListener.java
@@ -54,7 +54,7 @@ public class PlayerMoveListener implements Listener {
                             GameManager.get().playersFinished.add(e.getPlayer());
                             Scoreboard.scoreboards.forEach(Scoreboard::update);
                             int finishedCount = GameManager.get().playersFinished.size();
-                            if(finishedCount == 1) {
+                            if(finishedCount == 1 && Bukkit.getOnlinePlayers().size() != 1) {
                                 GameManager.get().playerCoinManager().addCoins(e.getPlayer(), 300);
                                 e.getPlayer().sendTitlePart(TitlePart.TITLE, Component.text("§6+300"));
                                 GameManager.get().startDroppingPhaseCooldown();
